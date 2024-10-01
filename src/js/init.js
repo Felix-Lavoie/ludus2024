c6b809abde4ebee0bc341d6cc2288a16a3ecd26f
Felix-Lavoie <112128712+Felix-Lavoie@users.noreply.github.com>
2024-10-01 16:48:51 -0400

    activated gravity in init

--- src/js/init.js
+++ src/js/init.js
@@ -1,8 +1,15 @@
 const config = {
     type: Phaser.AUTO,
     parent: "canvas-wrapper",
+    pixelArt: true,
     width: 800,
     height: 400,
-    scene: [Acceuil, Jeu, How, Credit, Partie_termine, Victoire]
+    scene: [Acceuil, Jeu, How, Credit, Partie_termine, Victoire],
+    physics: {
+        default: "arcade",
+        arcade: {
+          debug: true
+        }
+    }
 };
 const game = new Phaser.Game(config);
