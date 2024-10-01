b98e380b7533022e5f637bb6bfda5bfb2df5b1c4
Felix-Lavoie <112128712+Felix-Lavoie@users.noreply.github.com>
2024-10-01 17:16:36 -0400

    added all assets + some code

--- src/js/scenes/Jeu.js
+++ src/js/scenes/Jeu.js
@@ -7,7 +7,9 @@ class Jeu extends Phaser.Scene {
         this.load.image('bgF', './src/img/tiles/tiles/Assets/Background_2.png')
         this.load.image('bgC', './src/img/tiles/tiles/Assets/Background_1.png')
         this.load.image('btn', './src/img/ui/01_Flat_Theme/Sprites/UI_Flat_Bar01a')
-        this.load.image('player', './src/img/ui/01_Flat_Theme/Sprites/UI_Flat_Bar10a.png')
+        this.load.image('player', './src/img/characters/3_SteamMan/SteamMan')
+        //temp
+        this.load.image('ground', 'https://assets.codepen.io/9367036/Platform%2848x48%29.png')
     }
   
     create() {
@@ -21,11 +23,14 @@ class Jeu extends Phaser.Scene {
               this.scene.start("acceuil");
         });
         // Joueur
-        this.player = this.physics.add.sprite(config.width / 2, 0, "player");
+        this.player = this.physics.add.sprite(300, 300, "player");
         this.player.setBounce(0);
         this.player.setSize(10, 18);
         this.player.setOffset(10, 8);
         this.player.body.setGravityY(1000);
+        //temp floor
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(300, 530, "ground").setScale(2).refreshBody();
     }
   
     update() {
